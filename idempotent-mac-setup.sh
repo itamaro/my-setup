7812f17243b03bebbcacfb9c5200598308863d4d
Itamar Ostricher <itamarost@gmail.com>
2017-11-20 11:04:55 +0200

    Add an idempotent Mac setup script for regular execution

---
+++ idempotent-mac-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script for keeping existing Mac up to date (IDEMPOTENT!)
+
+if [ "$( uname -s )" != "Darwin" ]; then
+    echo "Not running on OS X!" 1>&2
+    exit 1
+fi
+
+echo "Update Homrbrew"
+brew update
+
+./osx-config.sh
