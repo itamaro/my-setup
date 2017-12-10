a97492e33c209c9f17a0541ef1b5d97c1413c6b8
Itamar Ostricher <itamarost@gmail.com>
2017-12-10 08:09:07 +0200

    Refactor list-update script

--- mbp2015/update-brew-lists.sh
+++ mbp2015/update-brew-lists.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-get_script_dir () {
-     SOURCE="${BASH_SOURCE[0]}"
-     # While $SOURCE is a symlink, resolve it
-     while [ -h "$SOURCE" ]; do
-          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-          SOURCE="$( readlink "$SOURCE" )"
-          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
-          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-     done
-     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-     echo "$DIR"
-}
-
-BASE_DIR="$( get_script_dir )"
-cd $BASE_DIR
-
 echo "Save versions of installed Homebrew & Cask"
 brew --version > homebrew.version
 brew cask --version > homebrew.cask.version
