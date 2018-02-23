428ba120b1caf9735d8329c70ee197778e8e6e1b
Itamar Ostricher <itamarost@gmail.com>
2018-02-23 14:32:31 +0200

    Base for a "quick" idempotent Mac setup script

---
+++ quick-idempotent-mac-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Script for keeping existing Mac up to date (IDEMPOTENT & FAST)
+
+if [ "$( uname -s )" != "Darwin" ]; then
+    echo "Not running on OS X!" 1>&2
+    exit 1
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BACKUP_DIR="$SCRIPT_DIR/.backup"
+
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p "$BACKUP_DIR"
+fi
+
+install_dotfile() {
+  src="$1"
+  dst="$2"
+  dst_dir="$( dirname $dst )"
+  if [ "$src" != "$( readlink $dst )" ]; then
+    if [ -f "$dst" ] || [ -d "$dst" ] || [ -L "$dst" ]; then
+      echo "Moving existing $dst to $BACKUP_DIR"
+      mv "$dst" "$BACKUP_DIR"
+    fi
+    if [ ! -d "$dst_dir" ]; then
+      mkdir -p "$dst_dir"
+    fi
+    echo "Installing $dst from dotfiles"
+    ln -s "$src" "$dst"
+  fi
+}
