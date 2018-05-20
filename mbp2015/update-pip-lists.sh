3eb9b6cac77f63bf213f3e6e8b010b8dc54de98c
Itamar Ostricher <itamarost@gmail.com>
2018-05-20 08:13:23 +0300

    Explicitly save versions of Python & Pip, 2 & 3

--- mbp2015/update-pip-lists.sh
+++ mbp2015/update-pip-lists.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Save versions of installed Python & Pip"
-python -V &> python.version
-pip -V > pip.version 2> /dev/null
+echo "Save versions of installed Python & Pip, 2 & 3"
+python3 -V &> python3.version
+pip3 -V > pip3.version 2> /dev/null
+python2 -V &> python2.version
+pip2 -V > pip2.version 2> /dev/null
