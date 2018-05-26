7b446a0f0705844ae84bca843829fc0cf4788736
Itamar Ostricher <itamarost@gmail.com>
2018-05-26 21:42:48 +0300

    Save list of installed pip3 packages

--- mbp2015/update-pip-lists.sh
+++ mbp2015/update-pip-lists.sh
@@ -5,3 +5,6 @@ python3 -V &> python3.version
 pip3 -V > pip3.version 2> /dev/null
 python2 -V &> python2.version
 pip2 -V > pip2.version 2> /dev/null
+
+echo "Save list of installed pip3 packages"
+pip3 freeze > pip3.freeze 2> /dev/null
